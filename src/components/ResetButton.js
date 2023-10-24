9f16f6db754ceff291f2493f833889a7c5dc2b91
reshni <reshni@1975.gmail.com>
2023-10-24 13:03:05 +0530

    First commit created a simple tic tac toe game

---
+++ src/components/ResetButton.js
@@ -0,0 +1,8 @@
+//rafc
+import React from 'react'
+import "./ResetButton.css"
+export const ResetButton = ({ resetBoard}) => {
+  return (
+    <button className="reset-btn" onClick={resetBoard} > Reset</button>
+  )
+}
